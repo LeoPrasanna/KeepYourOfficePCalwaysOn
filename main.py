af17355ba2b128b77fafacec14f4bf0e0d73bb11
yempap <yempadaprasanna91@gmail.com>
2020-12-26 19:05:54 +0530

    Version3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import datetime
 import os
+import sys
 
 #working hours are pre-defined as 9am to 6pm : set your hours according your shift hrs
 try:
@@ -12,8 +13,8 @@ try:
         if now >= tminus9 and now <=tminus18:
            print('Alive Feature started')
            for i in range(0,3):
-               file1 = open("myfile.txt", "a") 
-               file1.write('yo yo') 
+               file1 = open("myfile.txt", "w") 
+               file1.write('RandomText') 
                file1.close()              
                time.sleep(300)
         else:
@@ -26,6 +27,8 @@ try:
         
 
 
-except:
-    pass
+except Exception as err:
+    print(err.__context__)
+    print()
+    print(err.__cause__)
 
